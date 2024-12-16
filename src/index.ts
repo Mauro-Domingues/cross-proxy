17e9ef3e56e009b9124c266841c9045574b91c6e
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-12-16 10:08:39 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { IWritePacketDTO as IBaseMessageDTO } from '@interfaces/IWritePacketDTO';
+export { IMessageOptionsDTO } from '@interfaces/IMessageOptionsDTO';
+export { IPatternDTO } from '@interfaces/IPatternDTO';
 export { IModel } from '@interfaces/IModel';
+export { isType } from '@utils/isType';
 export { Proxy } from '@core/Proxy';
